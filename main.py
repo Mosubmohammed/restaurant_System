0095bb10bd8f1ae1a548b18130e0a90dc280ed75
Mosubmohammed <mosup.050@gmail.com>
2023-11-18 19:21:33 +0300

    ,

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-menuList={
-    
-    }
+import random
+
+
+menuList={}
 
 print('welcome to our restaurant')
 while True:
@@ -13,24 +14,32 @@ while True:
     print('6-view orders')
     print('7-Exit the System')
     Choice=input('Enter ur choice: ')
+    Char='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    number1='1 2 3 4 5 6 7 8 9 '
+    result=''
+    for i in range(0,2):
+        Char.random()
+        result+=random.random(Char)
+        result+=random.random(number1)
+    print(result)    
     if Choice == '1':
-        id=input('Enter the item ID')
         name=input('Enter the item name')
         desc=input('Enter the item description')
         price=input('Enter the item price')
         
-        
-        print(menuList)
-    elif Choice == '2':
-        price('2')
-    elif Choice == '3':
-        price('3')
-    elif Choice =='4':
-        price('4')
-    elif Choice == '5':
-        price('5')
-    elif Choice == '6':
-        price('6')
-    elif Choice == '7':
-        print('7')                    
+      
+          
+
+    # elif Choice == '2':
+    #     pass
+    # elif Choice == '3':
+    #     price('3')
+    # elif Choice =='4':
+    #     price('4')
+    # elif Choice == '5':
+    #     price('5')
+    # elif Choice == '6':
+    #     price('6')
+    # elif Choice == '7':
+    #     print('7')                    
 
