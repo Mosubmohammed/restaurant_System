2aebcb90de5e7b3040f57967c9f000f0289a963c
Mosubmohammed <mosup.050@gmail.com>
2023-11-20 18:27:28 +0300

    .

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ menuList={}
 print('welcome to our restaurant')
 print('--'*20)
 isWhile=True
+orderList=[]
+orderDic={}
 while isWhile:
     print('_'*20)
     print('1-add a item')
@@ -32,32 +34,38 @@ while isWhile:
         while True:
             name=input('Enter the item name :')
             desc=input('Enter the item description :')
-            price=input('Enter the item price :')
+            while True:
+                try:
+                    price=int(input('Enter the item price :'))
+                    break
+                except Exception as e:
+                    print('please enter a valid number')
+                    continue
+            
             menuDic={'name':name, 'description':desc , 'price':price}
             menuList[result]=menuDic
             print(menuList)
             break
           
     elif Choice == '2':
-       
             search = input('Enter the ID of the item: ')
-            try:
-                if search in menuList: 
-                    print('Here is the item:')
-                    print('#id:', search)
-                    print('  - Name:', menuList[search]['name'])
-                    print('  - Description:', menuList[search]['description'])
-                    print('  - Price:', menuList[search]['price'])
-                else:
-                    print('Sorry, the item is not in the menu')
-            except KeyError:
-                print('sorry the item is not in the menu')
+            while True:
+                try:
+                 if search in menuList: 
+                        print('Here is the item:')
+                        print('#id:', search)
+                        print('  - Name:', menuList[search]['name'])
+                        print('  - Description:', menuList[search]['description'])
+                        print('  - Price:', menuList[search]['price'])
+                        break
+                except Exception as e:
+                    print('sorry the item is not in the menu')
 
     elif Choice == '3':
          Remove1=input('Enter the Id of the item to delete it') 
          if Remove1 in menuList.keys():
             menuList.pop(Remove1)
-            print('you deleted the item')
+            print('successfully deleted')
          else:
              print('there is something wrong try again') 
 
@@ -71,29 +79,43 @@ while isWhile:
             print('  -','price:',menuList[i]['price'])
     elif Choice == '5':
         print('--'*20)
-        orderList=[]
-        orderDic={}
+        
         userinput1=input('please enter ur name :')
-        userinput2=input('please enter ur phone :')
-        userinput3=input('please enter the ID of the item')
-        if userinput3 in menuList:
-            orderDic={
-                 'name':userinput1,
-                 'phone':userinput2,
-                 'item':userinput3
-            }
-            orderList.append(orderDic)
-        else:
-            print('the item is not in the menu')     
+        while True:
+            try:
+                userinput2=int(input('please enter ur phone :'))
+                break
+            except Exception as e:
+                print('please enter a valid number')
+                continue       
+        userinput3=input('please enter the ID of the item :')
+        while True:
+            if userinput3 in menuList:
+                orderDic={
+                    'name':userinput1,
+                    'phone':userinput2,
+                    'item':userinput3
+                }
+                break
+            else:
+                print('the item is not in the menu')
+                userinput3=input('please enter the ID of the item again')
+        orderList.append(orderDic)
+        print('thank you for adding a order')
+             
 
     elif Choice == '6':
             print('--'*20)
             for i in orderList:
-               print('#id:',key)
-               print('  -','name:',orderList[i]['name'])
-               print('  -','description:',orderList[i]['phone'])
-               print('  -','price:',orderList[i]['item'])
-        
+               print('----------')
+               print('  -','name:',i['name'])
+               print('  -','phone:',i['phone'])
+               print('  -','item ID:',i['item'])
+               print('---- Item details:')
+               print('    - Name:', menuDic['name'])
+               print('    - Description:', menuDic['description'])
+               print('    - Price:', menuDic['price'])
+                        
     elif Choice == '7':
         
         print('byee thanks for visiting our restraint') 
